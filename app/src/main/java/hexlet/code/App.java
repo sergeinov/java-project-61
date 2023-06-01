744111badd66be0c072436fd9d9c123232af8768
Sergei <test@gmail.com>
2023-06-02 01:55:08 +0500

    implementation main logic choising game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,28 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        var sc = new Scanner(System.in);
+        var invitetion = "Please enter the game number and press Enter.";
+        var choice = """
+                1 - Greet
+                0 - Exit
+                """;
 
-        Cli.askName();
+        System.out.println(invitetion);
+        System.out.println(choice);
+
+        var answer = sc.nextInt();
+        switch (answer) {
+            case 1:
+                System.out.println("Your choice: " + answer);
+                Cli.askName();
+                break;
+            default:
+                System.out.println("Unknown game");
+        }
+
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Cli {
 
     public static void askName() {
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
 
         Scanner sc = new Scanner(System.in);
