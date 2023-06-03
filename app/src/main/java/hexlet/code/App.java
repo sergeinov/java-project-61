0d44c213e2dea64c176425e885eea040ecb10450
Sergei <test@gmail.com>
2023-06-03 13:01:20 +0500

    update main class - App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,28 +1,40 @@
 package hexlet.code;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        var sc = new Scanner(System.in);
+        var scanner = new Scanner(System.in);
         var invitetion = "Please enter the game number and press Enter.";
+        var choiceAnswer = "Your choice: ";
+        var unknownAnswer = "Unknown game";
         var choice = """
                 1 - Greet
+                2 - Even
                 0 - Exit
                 """;
 
         System.out.println(invitetion);
         System.out.println(choice);
-
-        var answer = sc.nextInt();
-        switch (answer) {
-            case 1:
-                System.out.println("Your choice: " + answer);
-                Cli.askName();
-                break;
-            default:
-                System.out.println("Unknown game");
+        try {
+            var answer = scanner.nextInt();
+            System.out.println(choiceAnswer + answer);
+            switch (answer) {
+                case 1:
+                    Greet.greetUser();
+                    break;
+                case 2:
+                    Even.isEven();
+                    break;
+                case 0:
+                    System.out.println("Good bye!");
+                    break;
+                default:
+                    System.out.println(unknownAnswer);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println(unknownAnswer);
         }
-
     }
 }
