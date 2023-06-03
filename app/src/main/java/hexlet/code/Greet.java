23105ac75b022a1ee24a518f70a426a0bac4d379
Sergei <test@gmail.com>
2023-06-03 13:00:30 +0500

    change logic for Cli to Greet class

--- app/src/main/java/hexlet/code/Greet.java
+++ app/src/main/java/hexlet/code/Greet.java
@@ -2,16 +2,17 @@ package hexlet.code;
 
 import java.util.Scanner;
 
-public class Cli {
+public class Greet {
+    private static Scanner scanner = new Scanner(System.in);
 
-    public static void askName() {
+    public static String greetUser() {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
 
-        Scanner sc = new Scanner(System.in);
-        String answerName = sc.nextLine();
-
+        String answerName = scanner.nextLine();
         System.out.println("Hello, " + answerName + "!");
+
+        return answerName;
     }
 
 }
