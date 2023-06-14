5dc9d9cbf73fc5cc25b7b44ecdaf127db4ae9669
Sergei <test@gmail.com>
2023-06-15 01:06:00 +0500

    fix errors autotests. Fix spaces in questions and answers

--- app/src/main/java/hexlet/code/games/Greet.java
+++ app/src/main/java/hexlet/code/games/Greet.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class Greet {
     private static Scanner scanner = new Scanner(System.in);
-    private static String userName = "";
+    private static String userName;
 
     public static String greetUser() {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name?\s");
+        System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
 
         userName = scanner.nextLine();
         System.out.println("Hello, " + userName + "!");
